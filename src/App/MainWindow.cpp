d5d2db83b6d414572ed4ab9ff32bee4b11a39715
Dênnis José da Silva <dennisjosesilva@gmail.com>
2023-01-11 18:07:24 +0100

    Open image dialog implementation.

--- src/App/MainWindow.cpp
+++ src/App/MainWindow.cpp
@@ -4,6 +4,14 @@
 #include <QMenuBar>
 #include <QAction>
 
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QImage>
+#include <QImageWriter>
+#include <QImageReader>
+#include <QByteArrayList>
+#include <QMessageBox>
+
 #include <QApplication>
 #include <QDebug>
 
@@ -18,6 +26,30 @@ MainWindow::MainWindow()
   setCentralWidget(g);
 }
 
+static void initializeImageFileDialog(QFileDialog &dialog, QFileDialog::AcceptMode acceptMode)
+{
+  static bool firstDialog = true;
+
+  if (firstDialog) {
+    firstDialog = false;
+    const QStringList pictureLocations = 
+      QStandardPaths::standardLocations(QStandardPaths::PicturesLocation);
+    dialog.setDirectory(pictureLocations.isEmpty() ? QDir::currentPath() : pictureLocations.last());
+  }
+
+  QStringList mimeTypeFilters;
+  const QByteArrayList supportedMimeTypes = acceptMode == QFileDialog::AcceptOpen ? 
+    QImageReader::supportedMimeTypes() : QImageWriter::supportedMimeTypes();
+  
+  for (const QByteArray &mimeTypeName : supportedMimeTypes)
+    mimeTypeFilters.append(mimeTypeName);
+
+  dialog.setMimeTypeFilters(mimeTypeFilters);
+  dialog.selectMimeTypeFilter("image/x-portable-greymap"); // PGM
+  dialog.setAcceptMode(acceptMode);
+}
+
+
 void MainWindow::createMenus()
 {
   QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
@@ -29,5 +61,13 @@ void MainWindow::createMenus()
 
 void MainWindow::openImageAct_onAction()
 {
-  qDebug() << "open Image";
+  QFileDialog dialog(this, tr("Open File"));
+  initializeImageFileDialog(dialog, QFileDialog::AcceptOpen);
+
+  bool accepted = dialog.exec() == QDialog::Accepted;
+
+  if (accepted == dialog.selectedFiles().count() > 0) {
+    const QString filename = dialog.selectedFiles().constFirst();    
+    qDebug() << filename <<  "chosen to be loaded";
+  }
 }
